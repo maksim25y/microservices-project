80cbdf0f0ab8de7cadb5377be80b0161838bbe88
maksim25y <mudanov515@gmail.com>
2025-01-10 12:02:39 +0300

    added base k8s and building image for api-gateway service 2

--- api-gateway/src/main/java/ru/mudan/ApiGatewayApplication.java
+++ api-gateway/src/main/java/ru/mudan/ApiGatewayApplication.java
@@ -1,7 +1,5 @@
 package ru.mudan;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,10 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(ApiGatewayApplication.class);
-
     public static void main(String[] args) {
-        log.info("55");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 }
