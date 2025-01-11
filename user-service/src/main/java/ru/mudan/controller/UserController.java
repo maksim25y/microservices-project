a00110c3a26110054b80c076fd262e1e97965973
maksim25y <mudanov515@gmail.com>
2025-01-11 21:29:31 +0300

    added method for getting user by id and added workflow for user-service

--- user-service/src/main/java/ru/mudan/controller/UserController.java
+++ user-service/src/main/java/ru/mudan/controller/UserController.java
@@ -1,7 +1,22 @@
 package ru.mudan.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mudan.dto.user.UserResponse;
+import ru.mudan.service.UserService;
 
 @RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping("/{id}")
+    public UserResponse createUser(@PathVariable Long id) {
+        return userService.getById(id);
+    }
 }
--- user-service/src/main/java/ru/mudan/service/UserService.java
+++ user-service/src/main/java/ru/mudan/service/UserService.java
@@ -1,4 +1,23 @@
 package ru.mudan.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.mudan.dto.user.UserResponse;
+import ru.mudan.repositories.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserResponse getById(Long id) {
+        var user = userRepository.findById(id).orElseThrow();
+        return UserResponse.builder()
+                .id(user.getId())
+                .firstname(user.getFirstname())
+                .lastname(user.getLastname())
+                .email(user.getEmail())
+                .build();
+    }
 }
